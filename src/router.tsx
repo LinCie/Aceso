86493dcd7a3a375e7bf28d5f3b17234ed48e7752
LinCie <aldiofernanda@protonmail.com>
2024-10-10 22:58:30 +0700

    remove product page

--- src/router.tsx
+++ src/router.tsx
@@ -5,13 +5,11 @@ import {
 } from "react-router-dom";
 import Layout from "./components/layout/Layout";
 import HomePage from "./pages/HomePage";
-import ProductPage from "./pages/ProductPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Layout />}>
       <Route path="/" element={<HomePage />} index />
-      <Route path="/products" element={<ProductPage />} index />
     </Route>,
   ),
 );
