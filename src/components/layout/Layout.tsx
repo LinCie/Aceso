d577ccacd0ac30407a80730b066bbdd491d357db
LinCie <aldiofernanda@protonmail.com>
2024-10-11 21:28:07 +0700

    added footer

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,13 @@
 import { Outlet } from "react-router-dom";
 import aceso from "@/assets/images/aceso.png";
+import react from "@/assets/images/react.svg";
+import vite from "@/assets/images/vite.svg";
+import tailwind from "@/assets/images/tailwind.svg";
+import github from "@/assets/images/github.svg";
 
 function Layout() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <>
       <header className="fixed inset-x-0 top-0 z-[100] bg-background/70 text-foreground backdrop-blur">
@@ -52,7 +58,91 @@ function Layout() {
       <main>
         <Outlet />
       </main>
-      <footer></footer>
+      <footer className="bg-background py-8 text-foreground">
+        <div className="mx-auto flex max-w-screen-xl flex-col items-center gap-4 px-4">
+          <div>
+            <div className="flex select-none items-center">
+              <div className="mr-2">
+                <img
+                  alt="Aceso Logo"
+                  loading="lazy"
+                  width="30"
+                  height="30"
+                  decoding="async"
+                  src={aceso}
+                  className="size-10"
+                />
+              </div>
+              <div className="text-xl font-semibold">ACESO</div>
+            </div>
+          </div>
+          <p className="mx-auto max-w-2xl text-center text-base text-muted-foreground">
+            Tubuh Lebih Kuat, Hidup Lebih Hebat
+          </p>
+          {/* <ul className="hidden min-w-0 flex-1 items-center justify-center gap-8 md:flex">
+            <li>
+              <a
+                className="text-foreground hover:text-primary"
+                href="#hero-section"
+              >
+                Home
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-foreground hover:text-primary"
+                href="#product-section"
+              >
+                Products
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-foreground hover:text-primary"
+                href="#about-section"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                className="text-foreground hover:text-primary"
+                href="#contact-section"
+              >
+                Contacts
+              </a>
+            </li>
+          </ul> */}
+          <div className="flex flex-col items-center justify-center gap-1">
+            <p>Made with:</p>
+            <ul className="flex min-w-0 flex-1 items-center justify-center gap-2">
+              <li>
+                <a href="https://react.dev/" target="_blank">
+                  <img src={react} alt="React" className="size-6" />
+                </a>
+              </li>
+              <li>
+                <a href="https://vite.dev/" target="_blank">
+                  <img src={vite} alt="Vite" className="size-6" />
+                </a>
+              </li>
+              <li>
+                <a href="https://tailwindcss.com/" target="_blank">
+                  <img src={tailwind} alt="Tailwind" className="size-6" />
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="text-center text-sm text-muted-foreground">
+            © {currentYear} Aceso. All Rights Reserved.
+          </div>
+          <div>
+            <a href="https://github.com/LinCie/Aceso" target="_blank">
+              <img src={github} alt="Github" className="size-6" />
+            </a>
+          </div>
+        </div>
+      </footer>
     </>
   );
 }
